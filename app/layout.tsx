aa22edbc8e06b315e62359fc81bd7e922e013aaa
Orkun Tumer <tumerorkun@outlook.com>
2025-09-22 21:52:09 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Appibara",
+  description: "Appibara LTD",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${nunito.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-nunito grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <Image
+          className="hidden dark:block max-w-3xl w-full"
+          src="/appibara_logo_dark.png"
+          alt="Appibara Logo"
+          width={4091}
+          height={1025}
+        />
+        <Image
+          className="block dark:hidden max-w-3xl w-full"
+          src="/appibara_logo.png"
+          alt="Appibara Logo"
+          width={4091}
+          height={1025}
+        />
+        <h1 className="text-2xl">We're under construction. Please check back for an update soon.</h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        ©Copyright 2025, Appibara LTD
+      </footer>
+    </div>
+  );
+}
