d76e5df00f12453c50ed02f1c21833aa9fed8551
Appibara LTD <edahazaltumer@gmail.com>
2026-06-16 18:08:27 +0300

    feat: adds blog post

--- lib/blog.ts
+++ lib/blog.ts
@@ -253,7 +253,7 @@ const FULL_POSTS_LIST = [
   },
 ] as const
 
-export const POSTS: BlogPost[] = FULL_POSTS_LIST.slice(0, 16);
+export const POSTS: BlogPost[] = FULL_POSTS_LIST.slice(0, 17);
 
 export async function getPosts(): Promise<BlogPost[]> {
   return POSTS;
